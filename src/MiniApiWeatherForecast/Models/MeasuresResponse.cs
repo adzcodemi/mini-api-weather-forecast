e2434bbeed6672c374a99ae277bd574ee88dad5e
Adrian Lacanilao <adrian@ecuiti.com>
2024-03-09 02:29:54 +0000

    added feature station measures

---
+++ src/MiniApiWeatherForecast/Models/MeasuresResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace MiniApiWeatherForecast.Models
+{
+    public class MeasuresResponse
+
+    { 
+        public string Context { get; set; } = string.Empty;
+        public Meta Meta { get; set; } = default!;
+        public List<Items> Items { get; set; } = default!;
+    }
+}
