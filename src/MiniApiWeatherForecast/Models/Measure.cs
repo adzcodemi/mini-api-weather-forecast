838775383451b93e0734f75c4de89b2ad3b3becf
Adrian Lacanilao <adrian@ecuiti.com>
2024-03-09 02:18:12 +0000

    Implement Error response and handling

---
+++ src/MiniApiWeatherForecast/Models/Measure.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+
+namespace MiniApiWeatherForecast.Models
+{
+    public class Measure
+    {
+        [JsonProperty("@id")]
+        public string @Id { get; set; } = string.Empty;
+        public string Label { get; set; } = string.Empty;
+        public LatestReading LatestReading { get; set; } = default!;
+        public string Notation { get; set; } = string.Empty;
+        public string Parameter { get; set; } = string.Empty;
+        public string ParameterName { get; set; } = string.Empty;
+        public int Period { get; set; }
+        public string Qualifier { get; set; } = string.Empty;
+        public string Station { get; set; } = string.Empty;
+        public string StationReference { get; set; } = string.Empty;
+        public List<string> Type { get; set; } = default!;
+        public string Unit { get; set; } = string.Empty;
+        public string UnitName { get; set; } = string.Empty;
+        public string ValueType { get; set; } = string.Empty;
+    }
+}
---
+++ src/MiniApiWeatherForecast/Models/StationResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace MiniApiWeatherForecast.Models
+{
+    public class StationResponse
+    {
+        public string Context { get; set; } = string.Empty;
+        public Meta Meta { get; set; } = default!;
+        public Items Items { get; set; } = default!;
+    }
+}
